c9c1749a2fdd4bc84b7bd44b650738d8c5b91a4a
aesrael <israeladura@gmail.com>
2021-08-29 00:49:39 +0100

    init concurrent file upload

--- workers/ul.go
+++ workers/ul.go
@@ -1,10 +1,13 @@
 package workers
 
 import (
+	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"musical/drive"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/apex/log"
 )
@@ -22,28 +25,47 @@ func uploadTrack(dirName string) error {
 		return err
 	}
 
+	wg := new(sync.WaitGroup)
+	wg.Add(len(files))
+
+	anyFailed := false
+
 	for _, v := range files {
-		if !isMusicFile(v.Name()) {
-			continue
-		}
-		trackPath := filepath.Join(dirPath, v.Name())
-		file, err := ioutil.ReadFile(trackPath)
-		if err != nil {
-			return err
-		}
-
-		_, uploadErr := drive.UploadFileToDrive(file, v.Name())
-
-		if uploadErr != nil {
-			return err
-		}
-		log.Info("file uploded: " + v.Name())
-		os.RemoveAll(trackPath)
+		go asyncUpload(v, wg, dirPath, &anyFailed)
 	}
 
+	wg.Wait()
 	if err := RedisDB.Set(dirName, "done", 0).Err(); err != nil {
 		return err
 	}
 
 	return os.RemoveAll(dirPath)
+}
+
+func asyncUpload(v fs.FileInfo, wg *sync.WaitGroup, dirPath string, anyFailed *bool) {
+	defer wg.Done()
+
+	if !isMusicFile(v.Name()) {
+		return
+	}
+
+	trackPath := filepath.Join(dirPath, v.Name())
+	file, err := ioutil.ReadFile(trackPath)
+
+	if err != nil {
+		log.Error(fmt.Sprintf("upload error occured %s", err.Error()))
+		*anyFailed = true
+		return
+	}
+
+	_, uploadErr := drive.UploadFileToDrive(file, v.Name())
+
+	if uploadErr != nil {
+		log.Error(fmt.Sprintf("upload error occured %s", err.Error()))
+		*anyFailed = true
+		return
+	}
+
+	log.Info("file uploded: " + v.Name())
+	os.RemoveAll(trackPath)
 }
